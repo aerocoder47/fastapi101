342a444db614d91aba4321b86975a637b6bf7a03
aerocoder47 <aerocoder47@gmail.com>
2025-08-03 21:55:41 +0530

    middleware for cors added

--- app/main.py
+++ app/main.py
@@ -5,11 +5,22 @@ from sqlalchemy.orm import Session
 from app.routers import post, user, auth, vote
 from app.config import settings
 
+from fastapi.middleware.cors import CORSMiddleware
+
 # alchmey db filler
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(post.router)
